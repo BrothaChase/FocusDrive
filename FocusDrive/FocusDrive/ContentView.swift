9fc04349ba7916f4d3b2a8eaec8dae0fbc71ceb6
Chase <chasebrown70131@gmail.com>
2026-07-15 19:18:02 -0500

    First commit

---
+++ FocusDrive/FocusDrive/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  FocusDrive
+//
+//  Created by Chase brown on 7/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var progress: Double = 0.0
+    @State var carLatitude: Double = 37.7759
+    @State var carLongitude: Double = -122.4191
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
